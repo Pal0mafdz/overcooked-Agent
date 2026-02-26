3a86dd0bb91e439eee818c3b37c802915090f58a
alo-py <alondramtz.dev@icloud.com>
2026-02-25 20:32:59 -0600

    Se agrega icono de overcooked

--- src/scenes/app.py
+++ src/scenes/app.py
@@ -7,7 +7,12 @@ from src.scenes.game_scene import GameScene
 def run_app(ordenes: int):
     pygame.init()
     ventana = pygame.display.set_mode((ANCHO_GRID * TAM_CELDA, ALTO_GRID * TAM_CELDA))
-    pygame.display.set_caption("Agente Inteligente - Presiona 'R' para reiniciar")
+    pygame.display.set_caption("Overcooked Agent")
+    
+    # Set window icon
+    icono = pygame.image.load("assets/icono_overcooked.png")
+    pygame.display.set_icon(icono)
+    
     reloj = pygame.time.Clock()
 
     juego_activo = True
