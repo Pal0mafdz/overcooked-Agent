739f1abba8f242c6cf9f6466467f76d61504f080
alo-py <alondramtz.dev@icloud.com>
2026-03-14 20:18:59 -0600

    Correccion de errores en carpetas

--- assets/resources_tiled/calle_izq_arriba.tsx
+++ assets/resources_tiled/calle_izq_arriba.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="calle_izq_arriba" tilewidth="64" tileheight="64" tilecount="22" columns="11">
- <image source="../Documents/Carrera/IA/resources_pixelorama/calle_izq_arriba.png" width="704" height="128"/>
+ <image source="../resources_pixelorama/calle_izq_arriba.png" width="704" height="128"/>
 </tileset>
